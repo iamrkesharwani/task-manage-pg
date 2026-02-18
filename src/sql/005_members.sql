339b3c05518dd5d1c1c4baa07ef1b9f704957c06
Rahul Kesharwani <132988447+iamrkesharwani@users.noreply.github.com>
2026-02-18 18:04:34 +0530

    add sql

---
+++ src/sql/005_members.sql
@@ -0,0 +1,8 @@
+DROP TABLE project_members;
+
+CREATE TABLE members (
+  project_id INTEGER NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  role TEXT DEFAULT 'Member' CHECK (role IN ('Owner', 'Admin', 'Member', 'Viewer')),
+  PRIMARY KEY (project_id, user_id)
+);
